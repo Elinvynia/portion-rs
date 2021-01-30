e36037da78db7697abb3e3244bd7e43185263b8e
Elinvynia <59487684+Elinvynia@users.noreply.github.com>
2021-01-30 20:28:59 +0100

    Add CI

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,15 @@
+//! [![ci-badge][]][ci] [![docs-badge][]][docs] [![crate-version]][crate-link]
+//!
 //! # portion-rs
 //!
 //! A simple interval library inspired by Python's `portion`.
+//!
+//! [ci]: https://github.com/Elinvynia/portion-rs/actions?query=workflow%3ARust
+//! [ci-badge]: https://img.shields.io/github/workflow/status/Elinvynia/portion-rs/Rust/master?style=flat-square
+//! [docs]: https://docs.rs/portion-rs
+//! [docs-badge]: https://img.shields.io/badge/docs-online-5023dd.svg?style=flat-square
+//! [crate-link]: https://crates.io/crates/portion-rs
+//! [crate-version]: https://img.shields.io/crates/v/portion-rs.svg?style=flat-square
 
 #![forbid(unsafe_code)]
 #![warn(missing_debug_implementations)]
