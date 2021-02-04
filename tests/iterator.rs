b922b018b2f33c688e6a7ae49fe929282520dc91
Elinvynia <59487684+Elinvynia@users.noreply.github.com>
2021-02-04 08:58:30 +0100

    Add tests for iterator

---
+++ tests/iterator.rs
@@ -0,0 +1,37 @@
+use portion_rs::*;
+
+#[test]
+fn singleton() {
+    let x: Vec<u8> = Portion::singleton(2).into_iter().collect();
+    assert_eq!(x, vec![2]);
+}
+
+#[test]
+fn closed() {
+    let x: Vec<u8> = Portion::closed(2, 5).into_iter().collect();
+    assert_eq!(x, vec![2, 3, 4, 5]);
+}
+
+#[test]
+fn open() {
+    let x: Vec<u8> = Portion::open(2, 5).into_iter().collect();
+    assert_eq!(x, vec![3, 4]);
+}
+
+#[test]
+fn openclosed() {
+    let x: Vec<u8> = Portion::openclosed(2, 5).into_iter().collect();
+    assert_eq!(x, vec![3, 4, 5]);
+}
+
+#[test]
+fn closedopen() {
+    let x: Vec<u8> = Portion::closedopen(2, 5).into_iter().collect();
+    assert_eq!(x, vec![2, 3, 4]);
+}
+
+#[test]
+fn empty() {
+     let x: Vec<u8> = Portion::empty().into_iter().collect();
+     assert_eq!(x, vec![]);
+}
